c53914c4fe52ee8b6f3286094b97c204b4d53d8a
Dhannyphantom <64708060+Dhannyphantom@users.noreply.github.com>
2022-11-15 16:35:49 +0100

    Updated Expo version to SDK 47

--- App.js
+++ App.js
@@ -7,9 +7,6 @@ import * as MediaLibrary from "expo-media-library";
 import Otaku from "./src/Otaku";
 const loaderFunc = async () => {
   return await Font.loadAsync({
-    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
-    "open-sans-b1": require("./assets/fonts/OpenSans-Bold.ttf"),
-    "open-sans-b2": require("./assets/fonts/OpenSans-ExtraBold.ttf"),
     sen: require("./assets/fonts/sen/Sen-Regular.ttf"),
     "reglise-black": require("./assets/fonts/reglise/ReglisseBack-eZewm.otf"),
     reglise: require("./assets/fonts/reglise/Reglisse-0WOD9.otf"),
