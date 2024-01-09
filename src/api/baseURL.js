568ab9a3070ce4092f3313f919d6fd47c77cdc98
Dhannyphantom <youngskillzzz@gmail.com>
2024-01-09 21:33:24 +0100

    Changed baseUrl to online server route for deployment

--- src/api/baseURL.js
+++ src/api/baseURL.js
@@ -1,5 +1,5 @@
 export default {
-  uri: "http://192.168.43.236:9001",
+  // uri: "http://192.168.43.236:9001",
   // uri: "http://10.255.8.152:9001",
-  // uri: "https://weebo-server.onrender.com",
+  uri: "https://weebo-server.onrender.com",
 };
