a652426bdc3445462bab8526041ebd96d9662098
Dhannyphantom <youngskillzzz@gmail.com>
2024-05-21 21:54:26 +0100

    Changed app icon

--- app.config.js
+++ app.config.js
@@ -4,7 +4,7 @@ module.exports = {
     slug: "weebo",
     version: "1.0.0",
     orientation: "portrait",
-    icon: "./assets/icon.png",
+    icon: "./assets/icon512.png",
     userInterfaceStyle: "light",
     splash: {
       image: "./assets/splash.png",
